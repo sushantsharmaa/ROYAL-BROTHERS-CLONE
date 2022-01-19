f75ecff23d9fb41e2e557deae852591ab5ca589d
Kapish Singh <kapishsingh2209@gmail.com>
2022-01-20 00:07:55 +0530

    commit

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -24,22 +24,28 @@ export const Signup = () => {
     ) {
       alert("Please fill in all fields");
     } else {
-      let formsubmit = {
+      // now let's send this to the server
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var raw = JSON.stringify({
         email: form.email,
         number: form.number,
         password: form.password,
-      };
-      fetch("https://bikeapis.herokuapp.com/register", {
+      });
+
+      var requestOptions = {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formsubmit),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        });
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch("https://bikeapis.herokuapp.com/register", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+      // end call
     }
   };
   return (
@@ -77,51 +83,3 @@ export const Signup = () => {
 // "email":"kapish@gmail.com",
 // "number": "9999999965",
 // "password": "123456"
-/*
-const handleSubmit = (e) => {
-    if (form.password !== form.confirmPassword) {
-      alert("Passwords don't match");
-      return;
-    }
-    const sendForm = {
-      email: form.email,
-      number: form.number,
-      password: form.password,
-    };
-    fetch("https://bikeapis.herokuapp.com/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(sendForm),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-  };
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  "email": "kapish@gmail.com",
-  "number": "9999999965",
-  "password": "123456"
-});
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("http://localhost:2345/register", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
-
-*/
