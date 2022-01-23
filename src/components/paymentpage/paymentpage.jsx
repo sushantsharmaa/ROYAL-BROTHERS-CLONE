818ebf898694f15582d2554a4dcae745a7b24720
Sushant Sharma <sushant.x01@gmail.com>
2022-01-23 17:42:28 +0530

    Improve Payment Page CSS

--- src/components/paymentpage/paymentpage.jsx
+++ src/components/paymentpage/paymentpage.jsx
@@ -9,83 +9,58 @@ import { Paytm } from "./Paytm"
 import { useParams } from "react-router-dom"
 import { Zesto } from "./Zesto"
 import { Upi } from "./Upi"
-// import StripeCheckout from 'react-stripe-checkout';
-export const Paymentpage = () =>{
+export const Paymentpage = () => {
 
     const [show, setShow] = useState("")
     console.log('show:', show)
-    // const [data, setData] = useState("")
-    
 
-    let {total} = useParams()
-    
+    let { total } = useParams()
     const amount = +total
     console.log('amount:', amount)
 
     return <div>
-    
-    <div style={{display:"flex"}}>
-    
-    <div className="card main-card">
-               <div className="card-header fw-bolder">
-                  CHOOSE PAYMENT METHOD
-               </div>
-         
-       <div style={{display:"flex"}}>
-       <div>
-       
-       <div className="card child-card">
-       <div className="text-alignment">
-       <a onClick={()=> setShow("creditcard")} className="credit-card" href="#">Cradit Card</a>
-       <hr/>
-       <a  onClick={()=> setShow("debitcard")} className="credit-card" href="#">Debit Card</a>
-       <hr/>
-       <a onClick={()=>setShow("netbanking")} className="credit-card" href="#">Net Banking</a>
-       <hr/>
-       <a onClick={()=>setShow("upi")} className="credit-card" href="#">UPI Payment</a>
-       <hr/>
-       <img  onClick={()=>setShow("zesto")} style={{width:"30%"}} src="https://d36g7qg6pk2cm7.cloudfront.net/assets/zest-ba7b00f2280b8dccaf218f9583ac1fce72183ccf866769d548bfe887bdb877ae.png" alt="test"/>
-       <hr/>
-       
-       <img  onClick={()=>setShow("paytm")}  style={{width:"30%",}} src="https://d36g7qg6pk2cm7.cloudfront.net/assets/paytm_logo-fa816f9adc5494b20bd1d42dde2a740ecf907e3514f3a3ffab3f680fe0e80256.png" alt="test" />
-       
-    
-       </div>
-       </div>
-
-       
-       </div>
-
-       {show==="creditcard"?<CreditCard />:show==="debitcard"?<DebitCard/>:show==="paytm"?<Paytm/>:show==="zesto"?<Zesto/>:show==="upi"?<Upi/>:show==="netbanking"?<Netbanking/>:<CreditCard />}
-
-       </div>
-       
-      </div>
+        <div className="main">
+            <div className="card main-card">
+                    <div className=" child-card">
+                            <a onClick={() => setShow("creditcard")} className="credit-card" href="#">Cradit Card</a>
+                            <hr className="hr" />
+                            <a onClick={() => setShow("debitcard")} className="credit-card" href="#">Debit Card</a>
+                            <hr className="hr"/>
+                            <a onClick={() => setShow("netbanking")} className="credit-card" href="#">Net Banking</a>
+                            <hr className="hr"/>
+                            <a onClick={() => setShow("upi")} className="credit-card" href="#">UPI Payment</a>
+                            <hr className="hr"/>
+                            <img onClick={() => setShow("zesto")} style={{ width: "30%", margin: "auto" }} src="https://d36g7qg6pk2cm7.cloudfront.net/assets/zest-ba7b00f2280b8dccaf218f9583ac1fce72183ccf866769d548bfe887bdb877ae.png" alt="test" />
+                            <hr className="hr"/>
+                            <img onClick={() => setShow("paytm")} style={{ width: "30%", margin: "auto" }} src="https://d36g7qg6pk2cm7.cloudfront.net/assets/paytm_logo-fa816f9adc5494b20bd1d42dde2a740ecf907e3514f3a3ffab3f680fe0e80256.png" alt="test" />
+                    </div>              
+                <div className=" child-card-2" >
+                {show === "creditcard" ? <CreditCard /> : show === "debitcard" ? <DebitCard /> : show === "paytm" ? <Paytm /> : show === "zesto" ? <Zesto /> : show === "upi" ? <Upi /> : show === "netbanking" ? <Netbanking /> : <CreditCard />}
+                </div>
+            </div>
+            <div className="card secondry-card" >
+                <div className="card-header fw-bolder">
+                    SUMMERY
+                </div>
+                <div className="secandry-child">
+                    <span>Total Payable Amount</span>
+                    <h6>₹{amount}.00</h6>
+                </div>
+            </div>
 
 
-      <div className="card secondry-card" >
-      <div className="card-header fw-bolder">
-      SUMMERY
-   </div>
-       <div className="secandry-child">
-       <span>Total Payable Amount</span>
-       <h6>₹{amount}.00</h6>
-       </div>
-      </div>
-     
-      
-    </div>
+        </div>
 
-    <div style={{padding:"3%"}}>
-    <p><b>NOTE:</b>
-    Making Payments on RoyalBrothers.com is 100% safe. Your transaction is processed through a secure 
-    https internet connection based on secure socket layer technology. 
-    Your payment details are only used for processing the transaction and never stored.
-    </p>
-    
-    </div>
-    
-    
-    
+        <div style={{ padding: "3%" }}>
+            <p><b>NOTE:</b>
+                Making Payments on RoyalBrothers.com is 100% safe. Your transaction is processed through a secure
+                https internet connection based on secure socket layer technology.
+                Your payment details are only used for processing the transaction and never stored.
+            </p>
+
+        </div>
+
+
+
     </div>
 }
